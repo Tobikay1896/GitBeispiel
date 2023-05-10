238095be42f9cf50a91dbffabc4b0807bed0cbbd
Küster <fw4-bbc-u1@fh-h.de>
2023-05-10 17:01:01 +0200

    Mult umgesetzt

--- Main.java
+++ Main.java
@@ -7,27 +7,19 @@
  */
 public class Main
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Main
-     */
-    public Main()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
+    public static void main (String args[]){
+        int summe =0;
+        for(int i=1; i<= 10;++i)
+        {
+            summe++;
+        }
+        int mult = 1;
+        for(int i =2;1<=10;i++){
+            mult *=i;
+        }
+    }
+    
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
-}
+    
+
